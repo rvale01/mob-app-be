673d1fa356333691973f87f38773ff75b3b0e167
rvale01 <rvale09@gmail.com>
2023-01-26 23:18:07 +0100

    added all data

--- index.ts
+++ index.ts
@@ -4,9 +4,14 @@ import { initData, allUsers } from "./data"
 export const app = express()
 const PORT = 4000
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/home-page-data', (req: Request, res: Response) => {
   res.send(initData)
 })
+
+app.get('/all-users', (req: Request, res: Response) => {
+  res.send(allUsers)
+})
+
 
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
